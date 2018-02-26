cfd89664d42b00d2446cdaf62abdf96990f94ef5
Vinorcola <vinorcola@gmail.com>
2018-02-26 22:11:59 +0100

    Shortcut for form errors.

--- src/Controller.php
+++ src/Controller.php
@@ -2,7 +2,10 @@
 
 namespace Vinorcola\HelperBundle;
 
+use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller as BaseController;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormInterface;
 
 abstract class Controller extends BaseController
 {
@@ -12,5 +15,25 @@ abstract class Controller extends BaseController
     protected function saveDatabase(): void
     {
         $this->getDoctrine()->getManager()->flush();
+    }
+
+    /**
+     * Add an error in the given form part.
+     *
+     * @param FormInterface $form
+     * @param string        $messageKey
+     * @param string[]      $messageParameters
+     */
+    protected function addFormError(FormInterface $form, string $messageKey, array $messageParameters = []): void
+    {
+        if (!$this->container->has('translator')) {
+            throw new LogicException('Translator service must be registered as a public service.');
+        }
+
+        $form->addError(new FormError(
+            $this->container->get('translator')->trans($messageKey, $messageParameters),
+            $messageKey,
+            $messageParameters
+        ));
     }
 }
