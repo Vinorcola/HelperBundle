9f773f95abdb4895464c49d9d6349222f1d1d453
Vinorcola <vinorcola@gmail.com>
2020-02-15 11:49:01 +0100

    Change base controller.

--- src/Controller.php
+++ src/Controller.php
@@ -2,12 +2,12 @@
 
 namespace Vinorcola\HelperBundle;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller as BaseController;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\Form\FormInterface;
 use Vinorcola\HelperBundle\Model\TranslationModel;
 
-abstract class Controller extends BaseController
+abstract class Controller extends AbstractController
 {
     /**
      * @var TranslationModel
